91c5105942d000141e4562e0e0985c8e376fc1fc
venkatkrishna133 <venkatzine@gmail.com>
2023-03-23 15:48:31 +0530

    First inserting in user table

--- Controller/userController.js
+++ Controller/userController.js
@@ -24,18 +24,65 @@ const getUser = asyncHandler(async(req,res)=>{
 
 
 const createUser = asyncHandler(async (req, res) => {
-    console.log("The request body is:", req.body);
-    const { name, email, phone } = req.body;
-    console.log("The request body is:", req.body);
-    if (!name || !email || !phone) {
+    console.log("The request body is 1:", req.body);
+    const {
+      user_name,
+      user_account_code,
+      user_address_1,
+      user_address_2,
+      user_pin_code,
+      user_city,
+      user_state,
+      user_country,
+      user_primary_number,
+      user_secondary_number,
+      user_email_address,
+      user_L1_employee,
+      user_L2_employee,
+      user_GSTIN_number,
+      user_type_of_bussiness,
+      user_owner_name,
+      user_password
+    } = req.body;
+    
+    console.log("The request body is 2:", req.body);
+    if (!user_name || !user_account_code || !user_address_1 || !user_pin_code || !user_city || !user_state || !user_country || !user_primary_number || !user_email_address || !user_password) {
       res.status(400);
       throw new Error("All fields are mandatory");
+      console.log("The request body is 3:", req.body);
     }
-    console.log("The request body is:", req.body);
+    
+    // if (typeof user_name !== "string" || typeof user_account_code !== "string" || typeof user_address_1 !== "string" || typeof user_pin_code !== "string" || typeof user_city !== "string" || typeof user_state !== "string" || typeof user_country !== "string" || typeof user_primary_number !== "string" || typeof user_email_address !== "string" || typeof user_password !== "string") {
+    //   res.status(400);
+    //   throw new Error("Invalid data type");
+    // }
+    
+    // if (!name || !email || !phone) {
+    //   res.status(400);
+    //   throw new Error("All fields are mandatory");
+    // }
+    console.log("The request body is 4:", req.body);
     // Assuming you have already set up a MySQL connection pool or client
-    // await db.query('INSERT INTO users (user_name, user_account_code, user_address_1, user_address_2, user_pin_code, user_city, user_state, user_country, user_primary_number, user_secondary_number, user_email_address, user_L1_employee, user_L2_employee, user_GSTIN_number, user_type_of_bussiness, user_owner_name, user_password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [name, account_code, address1, address2, pin_code, city, state, country, primary_number, secondary_number, email_address, l1_employee, l2_employee, gstin_number, type_of_business, owner_name, password]);
-    await db.query('INSERT INTO users (name, email, phone) VALUES (?, ?, ?)', [name, email, phone]);
-    console.log("The request body is:", req.body);
+    // await db.query('INSERT INTO users (user_name, user_account_code, user_address_1, user_address_2, user_pin_code, user_city, user_state, user_country, user_primary_number, user_secondary_number, user_email_address, user_L1_employee, user_L2_employee, user_GSTIN_number, user_type_of_bussiness, user_owner_name, user_password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [user_name, user_account_code, user_address_1, user_address_2, user_pin_code, user_city, user_state, user_country, user_primary_number, user_secondary_number, user_email_address, user_L1_employee, user_L2_employee, user_GSTIN_number, user_type_of_business, user_owner_name, user_password]);
+    await db.query('INSERT INTO users (user_name, user_account_code, user_address_1, user_address_2, user_pin_code, user_city, user_state, user_country, user_primary_number, user_secondary_number, user_email_address, user_L1_employee, user_L2_employee, user_GSTIN_number, user_type_of_bussiness, user_owner_name, user_password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [user_name,
+      user_account_code,
+      user_address_1,
+      user_address_2,
+      user_pin_code,
+      user_city,
+      user_state,
+      user_country,
+      user_primary_number,
+      user_secondary_number,
+      user_email_address,
+      user_L1_employee,
+      user_L2_employee,
+      user_GSTIN_number,
+      user_type_of_bussiness,
+      user_owner_name,
+      user_password]);
+    // await db.query('INSERT INTO users (name, email, phone) VALUES (?, ?, ?)', [name, email, phone]);
+    console.log("The request body is 5:", req.body);
     res.status(201).json({ message: "User created successfully" });
   });
 
